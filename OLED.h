2975c71a810ca170e626a68f2b9351ded63b1616
YFHD <92370642+YFHD-osu@users.noreply.github.com>
2022-11-03 22:49:13 +0800

    Update OLED.h

--- OLED.h
+++ OLED.h
@@ -11,7 +11,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 class SSD1306 {
   public:
-    void initialization();
+    void init();
     void printline(String context);
     void sendbuffer();
     void clearbuffer();
@@ -20,41 +20,27 @@ class SSD1306 {
     int yPos = 14;
 };
 
-void SSD1306::initialization() {
+void SSD1306::init() {
   u8g2.begin();
-  u8g2.enableUTF8Print();    // 啟動 UTF8 支援
+  u8g2.enableUTF8Print();  // 啟動 UTF8 支援
   return;
 }
 
 void SSD1306::printline (String context) {
-  u8g2.setFont(u8g2_font_unifont_t_chinese1);  // 使用 chinese1字型檔
-  u8g2.setCursor(0, yPos);
+  u8g2.setFont(u8g2_font_unifont_t_chinese1); // 使用 chinese1字型檔
+  u8g2.setCursor(0, yPos); //自動換行
   u8g2.print(context);
   yPos += 16;
   return;
 }
 
-void SSD1306::sendbuffer () {
+void SSD1306::sendbuffer () {  //顯示暫存器內容
   u8g2.sendBuffer();
   return;
 }
 
-void SSD1306::clearbuffer () {
+void SSD1306::clearbuffer () { //清除暫存器內容
   yPos = 14;
   u8g2.clearBuffer();
   return;
 }
-
-//void updateOLED(double Temperature, int Humidity, float pm25){
-//  u8g2.setFont(u8g2_font_unifont_t_chinese1);
-//  u8g2.firstPage();
-//  do {
-//    u8g2.setCursor(0, 14);
-//    u8g2.print("溫度: " + String(Temperature) + "°C");
-//    u8g2.setCursor(0, 35);
-//    u8g2.print("濕度: " + String(Humidity) + "%");
-//    u8g2.setCursor(0, 56);
-//    u8g2.print("PM2.5: " + String(pm25) + "mg/m³");
-//    
-//  } while (u8g2.nextPage());
-//}
